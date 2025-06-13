b53035ddd2d3c92ed81f19925dbc160452ae87b4
Marius Biegger <biegger.marius@gmail.com>
2025-06-13 14:07:01 +0200

    changed pictures to a random ghibli one

--- js/data/profileContent.js
+++ js/data/profileContent.js
@@ -1,13 +1,5 @@
 export const profileImagePaths = [
-    "media/profiles/profile.jpg",
-    "media/profiles/profile1.jpg",
-    "media/profiles/profile2.jpg",
-    "media/profiles/profile3.jpg",
-    "media/profiles/profile4.jpg",
-    "media/profiles/profile5.jpg",
-    "media/profiles/profile6.jpg",
-    "media/profiles/profile7.jpg",
-    "media/profiles/profile8.jpg",
+    "media/profiles/profile.png",
 ];
 
 export const profileCompliments = [
