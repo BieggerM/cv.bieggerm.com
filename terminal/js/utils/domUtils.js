108db332b45d6304fd0c6044f2155a9342fc3c6a
Marius Biegger <biegger.marius@gmail.com>
2025-08-21 20:49:59 +0200

    removed clutter

--- terminal/js/utils/domUtils.js
+++
@@ -1,4 +0,0 @@
-export function stripHtml(html) {
-    const doc = new DOMParser().parseFromString(html, 'text/html');
-    return doc.body.textContent || "";
-}
--- terminal/js/utils/formatUtils.js
+++
@@ -1,6 +0,0 @@
-export function getUptime(startTime) {
-    const diff = Math.abs(new Date() - startTime);
-    const minutes = Math.floor(diff / 60000);
-    const seconds = Math.floor((diff % 60000) / 1000);
-    return `${minutes}m ${seconds}s`;
-}
